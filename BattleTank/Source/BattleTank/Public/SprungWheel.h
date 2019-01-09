125301a5d9675f7fe9d687f9f5e4ca0c67e00415
fengsi <fengsi727@qq.com>
2019-01-09 17:58:29 +0800

    Add suspension.

---
+++ BattleTank/Source/BattleTank/Public/SprungWheel.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+#include "SprungWheel.generated.h"
+
+UCLASS()
+class BATTLETANK_API ASprungWheel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASprungWheel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void SetupComponent();
+
+	UPROPERTY(VisibleAnywhere, Category = "Component")
+		UStaticMeshComponent *Wheel = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Component")
+		UPhysicsConstraintComponent *MassWheelConstraint = nullptr;
+};
